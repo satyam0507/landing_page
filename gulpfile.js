813638768f7d3e4b954f0cfb95da4778de402636
satyam0507 <satyam0507@gmail.com>
2016-11-30 20:06:54 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp  = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('default',function(){
+    nodemon({
+        script:'server.js'
+    });
+});
--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 'use strict';
+
+var express = require('express');
+var exphbs = require('express-handlebars');
+var route = require('./routes/route.js');
+
+var app = express();
+
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir:'hbs/layouts/',
+    partialsDir:['hbs/partials/','hbs/views/partials']
+});
+app.engine('.hbs',hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', 'hbs/views');
+
+app.use(express.static('static/'));
+
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
+
+
+route(app);
