648fe59f21bf7bdadd60038447d615fa26115644
satyam0507 <satyam0507@gmail.com>
2016-11-30 21:52:36 +0530

    project structure created

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,39 @@
 'use strict';
 
-var gulp  = require('gulp');
+var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default',function(){
+var configPath = require('./config/init.json');
+
+gulp.task('nodemon', function () {
     nodemon({
-        script:'server.js'
+        script: 'server.js'
     });
 });
+
+
+
+gulp.task('sass', function () {
+    return gulp.src(configPath.scssPath + configPath.scssFile)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(configPath.cssPath));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch(configPath.scssPath + '*.scss', ['sass']);
+});
+
+gulp.task('default',function(){
+    runSequence(
+        'sass',
+        'sass:watch',
+        'nodemon'
+    )
+})
